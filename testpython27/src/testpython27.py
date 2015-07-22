2e5f5f879603c278406e693e51c0cd5e937f4b39
shaily pal <shaily.pal.2@gmail.com>
2015-07-22 19:18:32 +0530

    running crawler

--- testpython27/src/testpython27.py
+++ testpython27/src/testpython27.py
@@ -3,8 +3,8 @@ import urllib2
 import re
 from bs4 import BeautifulSoup
 
-file1 = open('database.txt', 'w')
-file2 = open('scienceQuiz.txt', 'w')
+#file1 = open('database.txt', 'w')
+#file2 = open('scienceQuiz.txt', 'w')
 
 req = urllib2.Request('http://gktoday.in/')
 
@@ -20,20 +20,32 @@ html = web.read()
 links = re.findall('"((http|ftp)s?://.*?)"', html)
 
 #write the extracted URL's to file1
-file1.write('File containing URLS from gktoday.in\n\n\n')
-for tup in links:
-    for tup0 in tup:
-        file1.write(str(tup0))
-        file1.write("\n")
-        break
+
+
+with open('database.txt', 'w') as file1:
+    file1.write('File containing URLS from gktoday.in\n\n\n')
+    for tup in links:
+        for tup0 in tup:
+            file1.write(str(tup0))
+            file1.write("\n")
+            break
+    
+    
+    search_str = "objective-questions/science-technology"
+    with open('database1.txt', 'r') as file1:
+        for sub_str in file1 :
+            if search_str in sub_str:
+                print sub_str
+                
     
 #connect to science quiz URL
-html = urllib2.urlopen('http://www.gktoday.in/objective-questions/science-technology/')
+html = urllib2.urlopen(sub_str)
 soup = BeautifulSoup(html)
 
-file2.write('File containing single extracted URL html \n\n\n')
-file2.write(str(soup))
-file2.close()
+with open('scienceQuiz.txt', 'w') as file2:
+    file2.write('File containing single extracted URL html \n\n\n')
+    file2.write(str(soup))
+    file2.close()
 
 #import mmap
 #f = open('database.txt')
